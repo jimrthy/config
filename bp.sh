dce363bc16ea194d825ecfe0895333b782257aca
James Gatannah <jgatannah@vbridges.com>
2014-01-30 20:02:57 +0000

    Start debugging my homebuilder scripts.

--- bp.sh
+++ bp.sh
@@ -3,26 +3,37 @@
 source ./build.sh
 
 # Clone the webapp source
-cd
+cd ~
 mv webapp webapp.initial
+mkdir projects
+cd projects
 git clone ssh://Tarpon/usr/local/share/bp.git
-mv bp webapp
+cd ..
+ln -s projects/bp webapp
 sudo supervisorctl restart all
 
 # Clone the agent sources 
 
 # Spinnaker first
-cd agents/spinnaker/usr/local
+cd agents/spinnaker/usr/
+sudo chmod g+w local
+sudo chgrp bp local
+
 # TODO: This duplication should happen in a function
 # For that matter, it should really be a symlink...it's
 # really wasteful to have this sort of duplication.
 # In all honesty, we should set up the base agent image
 # then deploy the tarpon pieces we need individually.
+cd local
 sudo mv agent agent.original
 git clone ssh://Tarpon/usr/local/share/agent.git
 
 # Tarpon
-cd /home/bp/agents/tarpon/usr/local
+cd /home/bp/agents/tarpon/usr/
+sudo chmod g+w local
+sudo chgrp bp local
+
+cd local
 sudo mv agent agent.original
 git clone ssh://Tarpon/usr/local/share/agent.git
 
