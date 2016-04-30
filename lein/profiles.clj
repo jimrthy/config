692e677aa73ebea292d14b4ee71d40f6b8c36841
James Gatannah <james.gatannah@gmail.com>
2016-04-30 15:25:43 -0500

    Merge lein profile w/ "real" one

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,8 @@
 ;; TODO:
 ;; Have a :plugins vector.
-;; Add [lein-outdated "1.0.0"] to it
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}  ; Q: Will I ever find occasion to actually use this?
         :dependencies [[alembic "0.3.2"]   ; Q: what's this really for? A: modifying CLASSPATH at runtime
+                       [commons-logging "1.2"]
                        ;; We inherit this next through vinyasa.
                        ;; Shouldn't need to declare it
                        ;; Actually, this doesn't make sense in here. Its entire purpose
@@ -15,27 +15,28 @@
                        #_[im.chit/hara "2.1.11"]
                        ;; This should probably go away
                        ;; Q: Did hara replace this too?
-                       [im.chit/vinyasa "0.3.4" :exclusions [org.codehaus.plexus/plexus-utils]]
-                       [io.aviso/pretty "0.1.24"]
+                       [im.chit/vinyasa "0.4.3" :exclusions [org.codehaus.plexus/plexus-utils]]
+                       #_[io.aviso/pretty "0.1.24"]
                        [leiningen #= (leiningen.core.main/leiningen-version)  :exclusions [cheshire
                                                                                            com.fasterxml.jackson.core/jackson-core
                                                                                            com.fasterxml.jackson.dataformat/jackson-dataformat-smile
-                                                                                           common-logging
+                                                                                           commons-logging
                                                                                            commons-codec
-                                                                                           org.apache.httpcomponents/httpclient
-                                                                                           org.apache.httpcomponents/httpcore
-                                                                                           org.apache.maven.wagon/wagon-http
+                                                                                           #_org.apache.httpcomponents/httpclient
+                                                                                           #_org.apache.httpcomponents/httpcore
+                                                                                           #_org.apache.maven.wagon/wagon-http
                                                                                            org.apache.maven.wagon/wagon-http-shared4
                                                                                            org.apache.maven.wagon/wagon-provider-api
                                                                                            org.clojure/clojure
                                                                                            org.clojure/tools.cli
-                                                                                           #_org.clojure/tools.reader
+                                                                                           org.clojure/tools.reader
                                                                                            org.jsoup/jsoup
                                                                                            potemkin
                                                                                            slingshot]]
                        ;; Q: How many of any of the rest of these do I actually use?
                        ;; This one's gone
-                       #_[nrepl-inspect "0.3.0"]
+                       #_[nrepl-inspect "0.4.1"]
+                       [org.apache.maven.wagon/wagon-provider-api "2.10"]
                        [org.codehaus.plexus/plexus-utils "3.0.22"]
                        ;; I use this everywhere. But it doesn't belong in here
                        #_[org.clojure/tools.namespace "0.2.10"]
@@ -46,17 +47,17 @@
                        #_[ritz/ritz-nrepl-middleware "0.7.0"]
                        ;; This approach is long deprecated.
                        ;; See its README and put it into :aliases insted
-                       #_[slamhound "1.5.5"]]
-        :injections [(require '[vinyasa.inject :as inject])
+                       #_[slamhound "1.5.5"]
+                       [slingshot "0.12.2" :exclusions [org.clojure/clojure]]
+                       [spyscope "0.1.5" :exclusions [clj-time]]]
+        :injections [(require 'spyscope.core)
+                     (require '[vinyasa.inject :as inject])
                      ;; TODO: call install-pretty-exception
-                     (require 'io.aviso.repl)
+                     #_(require 'io.aviso.repl)
                      (inject/in
                       ;; Default injection ns is .
                       [vinyasa.inject :refer [inject [in inject-in]]]
-                      #_[vinyasa.lein :exclude [*project*]]
-                      #_[vinyasa.pull :all]
                       [alembic.still [distill pull]]  ; Q: what is/was this?
-                      ;; Actually, I think I want to keep something along these lines
                       [cemerick.pomegranate add-classpath add-dependencies get-classpath resources]
 
 
@@ -79,11 +80,11 @@
                   [com.jakemccrary/lein-test-refresh "0.14.0"]
                   [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.10" :exclusions [cheshire
-                                                     common-codec
-                                                     commons-codec
-                                                     org.clojure/clojure
-                                                     #_org.clojure/tools.reader
-                                                     slingshot]]
+                                                      common-codec
+                                                      commons-codec
+                                                      org.clojure/clojure
+                                                      #_org.clojure/tools.reader
+                                                      slingshot]]
                   [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
                   [lein-pprint "1.1.2"]
                   [mvxcvi/whidbey "1.3.0" :exclusions [org.clojure/clojure]]]
@@ -95,4 +96,4 @@
                   :print-meta true
                   :color-scheme {}
                   :print-color true}}
-  :repl {:plugins [[cider/cider-nrepl "0.11.0" :exclusions [org.clojure/java.classpath]]]}}
+  :repl {:plugins [[cider/cider-nrepl "0.12.0" :exclusions [org.clojure/java.classpath]]]}}
