385aecf0c8cff06f5c24fb70ddbda610e87d399b
James Gatannah <jgatannah@px.ftw>
2016-08-31 12:51:17 -0500

    lein profile: bump ancient dependencies
    Tweak several others to get it working again.
    
    Well, at least working enough to start a REPL in a basic project.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,60 +1,59 @@
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}  ; Q: Will I ever find occasion to actually use this?
-        :dependencies [[alembic "0.3.2"]   ; Q: what's this really for? A: modifying CLASSPATH at runtime
+        :dependencies [;; Q: what's this really for? A: modifying CLASSPATH at runtime
+                       [alembic "0.3.2" :exclusions [org.tcrawley/dynapath]]
                        [commons-logging "1.2"]
-                       ;; We inherit this next through vinyasa.
-                       ;; Shouldn't need to declare it
-                       ;; Actually, this doesn't make sense in here. Its entire purpose
-                       ;; in life is to split away from vinyasa. It's a general purpose
-                       ;; library, such as useful.
-                       ;; TODO: Verify that nothing else in here depends on it.
-                       ;; (that seems to be done)
-                       ;; (I'm pretty sure it doesn't, because my general profiles.clj
-                       ;; doesn't have this)
-                       #_[im.chit/hara "2.1.11"]
                        ;; TODO: Replace this with whatever replaced it
                        ;; Careful: 0.3.4 was working OK
                        ;; Q: Did hara replace this too?
-                       [im.chit/vinyasa "0.4.3" :exclusions [org.codehaus.plexus/plexus-utils]]
-                       #_[io.aviso/pretty "0.1.24"]
-                       [leiningen #= (leiningen.core.main/leiningen-version)  :exclusions [cheshire
-                                                                                           com.fasterxml.jackson.core/jackson-core
-                                                                                           com.fasterxml.jackson.dataformat/jackson-dataformat-smile
-                                                                                           commons-logging
-                                                                                           commons-codec
-                                                                                           #_org.apache.httpcomponents/httpclient
-                                                                                           #_org.apache.httpcomponents/httpcore
-                                                                                           #_org.apache.maven.wagon/wagon-http
-                                                                                           org.apache.maven.wagon/wagon-http-shared4
-                                                                                           org.apache.maven.wagon/wagon-provider-api
-                                                                                           org.clojure/clojure
-                                                                                           org.clojure/tools.reader
-                                                                                           org.jsoup/jsoup
-                                                                                           potemkin
-                                                                                           slingshot]]
+                       #_[im.chit/vinyasa "0.4.7" :exclusions [im.chit/hara.reflect
+                                                             org.clojure/clojure
+                                                             org.codehaus.plexus/plexus-utils
+                                                             org.slf4j/jcl-over-slf4j]]
+                       [leiningen #= (leiningen.core.main/leiningen-version) :exclusions [cheshire
+                                                                                          com.fasterxml.jackson.core/jackson-core
+                                                                                          com.fasterxml.jackson.dataformat/jackson-dataformat-smile
+                                                                                          #_commons-io
+                                                                                          commons-logging
+                                                                                          commons-codec
+                                                                                          #_org.apache.httpcomponents/httpclient
+                                                                                          #_org.apache.httpcomponents/httpcore
+                                                                                          #_org.apache.maven.wagon/wagon-http
+                                                                                          org.apache.maven.wagon/wagon-http-shared4
+                                                                                          org.apache.maven.wagon/wagon-provider-api
+                                                                                          org.clojure/clojure
+                                                                                          org.clojure/tools.reader
+                                                                                          org.jsoup/jsoup
+                                                                                          potemkin
+                                                                                          slingshot]]
                        ;; Q: How many of any of the rest of these do I actually use?
                        ;; This one's gone
                        #_[nrepl-inspect "0.4.1"]
                        [org.apache.maven.wagon/wagon-provider-api "2.10"]
-                       [org.codehaus.plexus/plexus-utils "3.0.22"]
+                       [org.codehaus.plexus/plexus-utils "3.0.24"]
                        ;; I use this everywhere. But it doesn't belong in here
                        #_[org.clojure/tools.namespace "0.2.10"]
                        ;; I know I have a lot of projects that transitively rely on this, but they really shouldn't
                        #_[org.clojure/tools.nrepl "0.2.12" :exclusions [org.clojure/clojure]]
-                       [pjstadig/humane-test-output "0.8.0"]
+                       [pjstadig/humane-test-output "0.8.1"]
                        ;; Q: Is there any point to this next one?
                        #_[ritz/ritz-nrepl-middleware "0.7.0"]
                        ;; This approach is long deprecated.
                        ;; See its README and put it into :aliases instead
                        #_[slamhound "1.5.5"]
-                       [slingshot "0.12.2" :exclusions [org.clojure/clojure]]]
-        :injections [(require '[vinyasa.inject :as inject])
+                       ;; Q: Do I actually use this anywhere/for anything?
+                       ;; A: leiningen does.
+                       ;; Q: Why am/was I overriding?
+                       #_[slingshot "0.12.2" :exclusions [org.clojure/clojure]]]
+        :injections [#_(require '[vinyasa.inject :as inject])
                      ;; TODO: call install-pretty-exception
                      #_(require 'io.aviso.repl)
-                     (inject/in
+                     #_(inject/in
                       ;; Default injection ns is .
                       [vinyasa.inject :refer [inject [in inject-in]]]
                       [alembic.still [distill pull]]  ; Q: what is/was this?
-                      [cemerick.pomegranate add-classpath add-dependencies get-classpath resources]
+                      ;; I still want this.
+                      ;; Q: Where did it go?
+                      #_[cemerick.pomegranate add-classpath add-dependencies get-classpath resources]
 
                       ;;; At least 90% certain that I don't want any of the rest of this
                       ;; Inject into clojure.core
@@ -71,7 +70,7 @@
                      (require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
         ;;:local-repo "repo"
-        :plugins [[com.jakemccrary/lein-test-refresh "0.14.0"]
+        :plugins [[com.jakemccrary/lein-test-refresh "0.16.0"]
                   [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
                   ;; Check for out-dated plugins in here using `lein ancient check-profiles`
                   [lein-ancient "0.6.10" :exclusions [cheshire
